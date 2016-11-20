556ed88038b340af15eb59bd4f9e4371d861005f
Ravi_Panyam <panyam.vihari@gmail.com>
2016-11-19 22:30:43 -0500

    Final Commit
    Final Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myApp=angular.module('myApp', [])
 myApp.controller("mainController",function ($scope){
-
+	$scope.showFullList = true;
 	$scope.index
 	
 	$scope.students=[
@@ -96,6 +96,9 @@ myApp.controller("mainController",function ($scope){
     }
 ]
 $scope.editStudent=function (std,index){
+	$scope.showEdit = true;
+	$scope.showFullList = false
+	
     $scope.index = index
 	$scope.selectedperson = $scope.students[index]
 	$scope.fname = $scope.selectedperson.fname
@@ -106,13 +109,18 @@ $scope.editStudent=function (std,index){
 	$scope.state= $scope.selectedperson.state
 	$scope.zip = $scope.selectedperson.zip
 	
-	console.log(std)
+}
+
+$scope.deleteStudent=function(index){
+	var newlist = $scope.students
+	newlist.splice(index, 1)
+	$scope.students = newlist
 	console.log(index)
 }
 
-
 $scope.updateStudent = function (){
-	
+	$scope.showEdit = false;
+	$scope.showFullList = true;
 	$scope.newstudent={}
 	$scope.newstudent.fname = $scope.fname
 	$scope.newstudent.lname = $scope.lname
@@ -124,5 +132,63 @@ $scope.updateStudent = function (){
 	
 	$scope.students[$scope.index] = $scope.newstudent
 	
+	$scope.newstudent={}
+	
+}
+$scope.addStudent=function(){
+	resetModelValues()
+	$scope.showAddScreen = true;
+	$scope.showFullList = false;
+	$scope.showEdit = false;
+}
+
+$scope.addNewStudentRecord=function(){
+	
+	$scope.newstudent={}
+
+	$scope.newstudent.fname = $scope.nFname
+	$scope.newstudent.lname = $scope.nLname
+	$scope.newstudent.tel = $scope.nTelephone;
+	$scope.newstudent.address = $scope.nAddress;
+	$scope.newstudent.city = $scope.nCity;
+	$scope.newstudent.state = $scope.nState;
+	$scope.newstudent.zip = $scope.nZipCode;
+	var length = $scope.students.length
+	$scope.students[length] = $scope.newstudent 
+	
+	
+	$scope.showAddScreen = false;
+	$scope.showFullList = true;
+	$scope.showEdit = false;
+}
+
+$scope.cancelAddingNewRecord=function(){
+	
+
+	$scope.showAddScreen = false;
+	$scope.showFullList = true;
+	$scope.showEdit = false;	
+}
+
+$scope.resetModelValues=function(){
+
+	$scope.newstudent.fname = ""
+	$scope.newstudent.lname = ""
+	$scope.newstudent.tel = ""
+	$scope.newstudent.address = ""
+	$scope.newstudent.city = ""
+	$scope.newstudent.state = ""
+	$scope.newstudent.zip = ""
+		
+	$scope.nFname = ""
+	$scope.nLname = ""
+	$scope.nTelephone = ""
+	$scope.nCity = ""
+	$scope.nState = ""
+	$scope.nLname = ""
+	$scope.nZipCode =""
+						
+			
+
 }
 })
